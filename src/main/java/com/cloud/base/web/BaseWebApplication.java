0fb2de5176b63f99a0bee4f8661456874e3ff7b0
Jon_China <503665965@qq.com>
2017-12-29 21:02:26 +0800

    fix(init):工程拆分初始化
    工程拆分初始化

---
+++ src/main/java/com/cloud/base/web/BaseWebApplication.java
@@ -0,0 +1,24 @@
+package com.cloud.base.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BaseWebApplication {
+
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseWebApplication.class, args);
+	}
+}
---
+++ src/main/java/com/cloud/base/web/config/LoginUserContext.java
@@ -0,0 +1,26 @@
+package com.cloud.base.web.config;
+
+import com.cloud.base.web.exception.UserAuthException;
+import com.cloud.base.web.model.LoginUser;
+import com.cloud.base.web.utils.EmptyChecker;
+
+/**
+ * 当前登录用户上下文
+ *
+ * @author Jon_China
+ * @create 2017/11/11
+ */
+public final class LoginUserContext {
+
+    private static ThreadLocal<LoginUser> userContext = new ThreadLocal<>();
+
+
+    public static void addLoginUserContext(LoginUser loginUser){
+        if(EmptyChecker.isEmpty(loginUser)){
+            throw new UserAuthException("非法用户");
+        }
+        userContext.set(loginUser);
+    }
+
+
+}
